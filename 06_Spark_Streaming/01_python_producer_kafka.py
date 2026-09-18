084bacbce0ae21de1143475030c19d45334d46b3
Vaibhav Kharate <vaibhav.s.kharate@gmail.com>
2026-09-18 22:04:03 +0530

    Add comments explaining the Kafka producer setup and Spark Structured Streaming application

--- 06_Spark_Streaming/01_python_producer_kafka.py
+++ 06_Spark_Streaming/01_python_producer_kafka.py
@@ -1,3 +1,9 @@
+# 06_Spark_Streaming/01_python_producer_kafka.py
+# in this file, we are creating a simple Flask application that acts as a Kafka producer. The application listens for POST requests at the '/send' endpoint, and when it receives a request, it sends the JSON data to a Kafka topic named 'test-topic'. The Kafka producer is configured to connect to a Kafka broker running at '
+# this code snippet demonstrates how to set up a basic Kafka producer using Flask and the kafka-python library. The producer is configured to serialize the data as JSON before sending it to the specified Kafka topic. The application can be tested using a curl command to send a POST request with JSON data.
+# kafka-python is a Python client for Apache Kafka, which allows you to produce and consume messages from Kafka topics. In this example, we use the KafkaProducer class to create a producer instance that connects to the specified Kafka broker and sends messages to the 'test-topic' topic. The value_serializer parameter is used to specify how the data should be serialized before sending it to Kafka, in this case, converting the data to JSON format.
+# kafka producer is a component that allows you to send messages to Kafka topics. In this example, we create a Kafka producer instance using the KafkaProducer class from the kafka-python library. The producer is configured with the bootstrap_servers parameter, which specifies the address of the Kafka broker to connect to. The value_serializer parameter is used to define how the data should be serialized before sending it to Kafka, in this case, converting the data to JSON format using json.dumps and encoding it as UTF-8.
+
 from flask import Flask, request, jsonify
 from kafka import KafkaProducer
 import json
--- 06_Spark_Streaming/02_read_from_kafka.py
+++ 06_Spark_Streaming/02_read_from_kafka.py
@@ -1,3 +1,7 @@
+# 06_Spark_Streaming/02_read_from_kafka.py
+# this code snippet demonstrates how to set up a basic Spark Structured Streaming application that reads data from a Kafka topic named 'test-topic'. The application connects to a Kafka broker running at 'ec2-3-145-208-232.us-east-2.compute.amazonaws.com:9092' and processes the incoming messages in real-time. The messages are expected to be in JSON format, and the application defines a schema to parse the JSON data into a structured DataFrame. The streaming query is configured to print the processed data to the console continuously, allowing for real-time monitoring of the incoming Kafka messages.
+# this Spark Structured Streaming application reads data from a Kafka topic named 'test-topic' and processes it in real-time. The application connects to a Kafka broker running at 'ec2-3-145-208-232.us-east-2.compute.amazonaws.com:9092' and expects the incoming messages to be in JSON format. A schema is defined to parse the JSON data into a structured DataFrame, allowing for easy manipulation and analysis of the data. The streaming query is configured to print the processed data to the console continuously, enabling real-time monitoring of the incoming Kafka messages.
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType
